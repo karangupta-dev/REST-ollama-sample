3750704d761dadda7f95d3392e32847ba5552b9b
Karan Gupta <karangupta.dev@gmail.com>
2025-09-05 18:57:46 -0500

    Added initial files for the project

---
+++ src/main/java/karan/gupta/dev/ollama_sample/OllamaSampleApplication.java
@@ -0,0 +1,13 @@
+package karan.gupta.dev.ollama_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/karan/gupta/dev/ollama_sample/OllamaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package karan.gupta.dev.ollama_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
